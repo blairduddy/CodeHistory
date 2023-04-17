e14a75a2a3d3cfb42ad767c945da090fc0ace28d
Blair Duddy <blairduddy@hotmail.co.uk>
2023-04-17 17:20:39 -0400

    Addition of Question file

---
+++ CodeHistory/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  CodeHistory
+//
+//  Created by Blair Duddy on 2023-04-17.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
