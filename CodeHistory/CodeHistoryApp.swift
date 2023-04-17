4b4b1ad348598da4a2b2fa6ecd9bc4db1d12a765
Blair Duddy <blairduddy@hotmail.co.uk>
2023-04-17 16:49:02 -0400

    Initial Commit

---
+++ CodeHistory/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryApp.swift
+//  CodeHistory
+//
+//  Created by Blair Duddy on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
